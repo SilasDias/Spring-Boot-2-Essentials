f9ed641f8fc3478d602b92661c4ee547543a7c24
silas.dias <silasdjd@gmail.com>
2021-01-27 17:24:53 -0300

    30 - Spring Data JPA Test pt 01

---
+++ src/test/java/academy/devdojo/SpringBoot2/repository/AnimeRepositoryTest.java
@@ -0,0 +1,35 @@
+package academy.devdojo.SpringBoot2.repository;
+
+import academy.devdojo.SpringBoot2.domain.Anime;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@DisplayName("Tests for Anime Repository")
+class AnimeRepositoryTest {
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("Save creates anime when Successful")
+    void save_PersistenteAnime_WhenSuccessful(){
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());
+
+    }
+
+    private Anime createAnime(){
+        return Anime.builder()
+                .name("Hajime no Ippo")
+                .build();
+    }
+
+}
